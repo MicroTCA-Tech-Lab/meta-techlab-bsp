7d157579fca1eece6d7bcf3c4e0bd6897fde62a0
Jan Marjanovic <jan.marjanovic@desy.de>
2021-01-12 17:33:15 +0100

    device-tree: add a xsct script to gen the app DTS

--- recipes-bsp/device-tree/device-tree.bbappend
+++ recipes-bsp/device-tree/device-tree.bbappend
@@ -1,7 +1,20 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
-SRC_URI += "file://system-user.dtsi"
+SRC_URI += " \
+	file://system-user.dtsi \
+	file://gen_app_amba_dts.tcl \
+"
 
 do_configure_append_damc-fmc2zup() {
+    # get the xsct files
+    XSCT_BIN_DIR=$(which xsct)
+    XSCT_DIR=$(dirname $(dirname $XSCT_BIN_DIR))
+
+    # generate .dts for the app
+    eval xsct -sdx -nodisp -eval 'source ${WORKDIR}/gen_app_amba_dts.tcl ${XSCT_DIR} ${WORKDIR} ${XSCTH_HDF}'
+    cp dts_app/zup_app.dts ${DT_FILES_PATH}/zup_app.dtsi
+
+    # append aux dts files (BSP, app PL) to the main dts
     echo '#include "system-user.dtsi"' >> ${DT_FILES_PATH}/system-top.dts
+    echo '#include "zup_app.dtsi"' >> ${DT_FILES_PATH}/system-top.dts
 }
---
+++ recipes-bsp/device-tree/files/gen_app_amba_dts.tcl
@@ -0,0 +1,90 @@
+
+# Copyright (c) 2021 Deutsches Elektronen-Synchrotron DESY
+
+# creates dts_app/zup_app.dts
+
+
+if {$argc != 3} {
+	puts "Usage: $argv0 XSCT_DIR WORKDIR"
+	exit 1
+}
+
+set XSCT_DIR [lindex $argv 0]
+set WORKDIR [lindex $argv 1]
+set HDF_OR_XSA_PATH [lindex $argv 2]
+
+puts "XSCT_DIR: $XSCT_DIR"
+puts "WORKDIR: $WORKDIR"
+puts "HDF_OR_XSA_PATH: $HDF_OR_XSA_PATH"
+
+
+source ${XSCT_DIR}/scripts/hsm/xillib_hw.tcl
+source ${XSCT_DIR}/scripts/hsm/xillib_sw.tcl
+
+hsi::set_repo_path $WORKDIR/git
+hsi::open_hw_design $HDF_OR_XSA_PATH
+
+set tree [hsi::create_dt_tree -dts_file zup_app.dts]
+set root_node [hsi::create_dt_node -name "/"]
+set bus_node [hsi::create_dt_node -name "amba_app" -label "amba_app" -unit_addr 0 -object $root_node]
+
+
+foreach cell [hsi::get_cells] {
+    set comp_name [hsi::get_property CONFIG.Component_Name [hsi::get_cells $cell]]
+    set is_in_app [string match "system_app*" $comp_name]
+    # puts "$is_in_app : $cell"
+
+    if {$is_in_app} {
+        set mem_ranges [hsi::get_mem_ranges [hsi::get_cells $cell]]
+
+        set mem ""
+        foreach mem_range $mem_ranges {
+            set mst_iface [hsi::get_property MASTER_INTERFACE $mem_range]
+            if {[string match "arm_lpd_m_axi" $mst_iface]} {
+                # puts "  $mst_iface"
+                set mem $mem_range
+            }
+        }
+
+        if {[expr {$mem ne ""}]} {
+
+			set base [string tolower [hsi::get_property BASE_VALUE $mem]]
+			set high [string tolower [hsi::get_property HIGH_VALUE $mem]]
+			set size [format 0x%x [expr {${high} - ${base} + 1}]]
+
+            if {[regexp -nocase {0x([0-9a-f]{9})} "$base" match]} {
+                set temp $base
+                set temp [string trimleft [string trimleft $temp 0] x]
+                set len [string length $temp]
+                set rem [expr {${len} - 8}]
+                set high_base "0x[string range $temp $rem $len]"
+                set low_base "0x[string range $temp 0 [expr {${rem} - 1}]]"
+                set low_base [format 0x%08x $low_base]
+                if {[regexp -nocase {0x([0-9a-f]{9})} "$size" match]} {
+                    set temp $size
+                    set temp [string trimleft [string trimleft $temp 0] x]
+                    set len [string length $temp]
+                    set rem [expr {${len} - 8}]
+                    set high_size "0x[string range $temp $rem $len]"
+                    set low_size  "0x[string range $temp 0 [expr {${rem} - 1}]]"
+                    set low_size [format 0x%08x $low_size]
+                    set reg "$low_base $high_base $low_size $high_size"
+                } else {
+                    set reg "$low_base $high_base 0x0 $size"
+                }
+            } else {
+                set reg "0x0 $base 0x0 $size"
+            }
+
+            set ip_name [hsi::get_property HIER_NAME [hsi::get_cells $cell]]
+            set comp_node [hsi::create_dt_node -name $ip_name -label $ip_name -unit_addr $base -object $bus_node]
+            hsi::utils::add_new_dts_param $comp_node "compatible" "generic-uio" string
+            hsi::utils::add_new_dts_param $comp_node "reg" "$reg" intlist
+        }
+    }
+}
+
+
+hsi::create_sw_design dt1 -os device_tree -proc psu_cortexa53_0
+hsi::generate_target -dir dts_app
+
