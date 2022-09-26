89a1fd0a89dfdaf9b224c63e9e5e908fc6ad60dc
Patrick Huesmann <patrick.huesmann@desy.de>
2022-09-26 13:03:24 +0200

    Autodetect FPGA variant & load correct bitstream

--- recipes-bsp/fpgautil-init/files/damc-fmc1z7io/fpgautil-init.sh
+++ recipes-bsp/fpgautil-init/files/damc-fmc1z7io/fpgautil-init.sh
@@ -1,3 +1,32 @@
-echo "fpgautil-init: downloading FPGA image"
-fpgautil -b /lib/firmware/base/damc_fmc1z7io_top.bit.bin -o /lib/firmware/base/base.dtbo
-echo "fpgautil-init: FPGA image download done"
+#!/bin/sh
+
+scriptname="fpgautil-init"
+
+get_chip_variant() {
+
+    chip_idcode=$(cat /sys/devices/soc0/soc_id)
+    case ${chip_idcode} in
+
+    "0x0c")
+        chip_variant="7z030"
+        ;;
+
+    "0x12")
+        chip_variant="7z035"
+        ;;
+
+    *)
+        echo "${scriptname}: Chip variant not found: unknown ID code of '${chip_idcode}'"
+        echo "${scriptname}: FPGA configuration aborted"
+        exit 1
+        ;;
+    esac
+
+    echo "${scriptname}: Detected FPGA chip variant '${chip_variant}'"
+}
+
+get_chip_variant
+
+echo "${scriptname}: Downloading FPGA image"
+fpgautil -b /lib/firmware/base/${chip_variant}/damc_fmc1z7io_top.bit.bin -o /lib/firmware/base/${chip_variant}/base.dtbo
+echo "${scriptname}: FPGA image download done"
--- recipes-bsp/fpgautil-init/files/damc-fmc2zup/fpgautil-init.sh
+++ recipes-bsp/fpgautil-init/files/damc-fmc2zup/fpgautil-init.sh
@@ -1,6 +1,32 @@
-# FIXME: Choose correct FPGA depending on machine & FPGA variant
+#!/bin/sh
 
-echo "fpgautil-init: downloading FPGA image"
-fpgautil -b /lib/firmware/base/damc_fmc2zup_top.bit.bin -o /lib/firmware/base/base.dtbo
-echo "fpgautil-init: FPGA image download done"
+scriptname="fpgautil-init"
 
+get_chip_variant() {
+
+    chip_idcode=$(dd if=/sys/bus/nvmem/devices/zynqmp-nvmem0/nvmem bs=4 count=1 skip=1 status=none | od -t x4 | cut -d' ' -f2 | head -n1)
+    case ${chip_idcode} in
+
+    "04740093")
+        chip_variant="zu11eg"
+        ;;
+
+    "14758093")
+        chip_variant="zu19eg"
+        ;;
+
+    *)
+        echo "${scriptname}: Chip variant not found: unknown ID code of '${chip_idcode}'"
+        echo "${scriptname}: FPGA configuration aborted"
+        exit 1
+        ;;
+    esac
+
+    echo "${scriptname}: Detected FPGA chip variant '${chip_variant}'"
+}
+
+get_chip_variant
+
+echo "${scriptname}: Downloading FPGA image"
+fpgautil -b /lib/firmware/base/${chip_variant}/damc_fmc2zup_top.bit.bin -o /lib/firmware/base/${chip_variant}/base.dtbo
+echo "${scriptname}: FPGA image download done"
