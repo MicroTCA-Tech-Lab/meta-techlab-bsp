9313f0830f9d8c4987cb68a253cb23764d50780d
Patrick Huesmann <patrick.huesmann@desy.de>
2023-03-22 14:50:00 +0100

    motctrl: Fix PL loading

--- recipes-bsp/fpgautil-init/files/damc-motctrl/fpgautil-init.sh
+++ recipes-bsp/fpgautil-init/files/damc-motctrl/fpgautil-init.sh
@@ -3,5 +3,5 @@
 scriptname="fpgautil-init"
 
 echo "${scriptname}: Downloading FPGA image"
-fpgautil -b /lib/firmware/base/test/design_1_wrapper.bit.bin -o /lib/firmware/base/test/base.dtbo
+fpgautil -b /lib/firmware/base/test/damc_motctrl_top.bit.bin -o /lib/firmware/base/test/base.dtbo
 echo "${scriptname}: FPGA image download done"
